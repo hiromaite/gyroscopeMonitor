623a85ab230d721eabfb0a97d8a0173b3865bcc1
Hiromasa Ito <x.raymcd@gmail.com>
2022-07-08 10:50:28 +0900

    [clean] setup, init, global var

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,20 @@
 #undef min
 AXP192 Axp;
 
+float accX = 0;
+float accY = 0;
+float accZ = 0;
+float gyroX = 0;
+float gyroY = 0;
+float gyroZ = 0;
+float temp = 0;
+
 void setup() {
-  // Initialize M5Core2, Don't use M5.begin() cuz it conflicts with BLE initialization.
-  Axp.begin();
-  M5.Lcd.begin();
+  M5.begin();
+  M5.IMU.Init();
+  M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setTextSize(3);
-  M5.Rtc.begin();
-  Serial.begin(115200);
+  M5.Lcd.setCursor(0, 0);
 
 }
 
