5c1a8114e5d5573f438736ef6dc431a880f31787
Hiromasa Ito <x.raymcd@gmail.com>
2022-07-08 12:08:57 +0900

    [update] add dataAcquisition, math, print

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,31 @@
 #undef min
 AXP192 Axp;
 
-float accX = 0;
-float accY = 0;
-float accZ = 0;
-float gyroX = 0;
-float gyroY = 0;
-float gyroZ = 0;
-float temp = 0;
+float accX, accY, accZ;
+float gyroX, gyroY, gyroZ;
+float pitch, roll, yaw;
+float my_pitch;
+float temp;
 
 void setup() {
   M5.begin();
   M5.IMU.Init();
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setTextSize(3);
-  M5.Lcd.setCursor(0, 0);
 
 }
 
 void loop() {
+  M5.update();
+  // data acquisition
+  M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
+  M5.IMU.getAccelData(&accX, &accY, &accZ);
+  M5.IMU.getAhrsData(&pitch, &roll, &yaw);
+  M5.IMU.getTempData(&temp);
+  my_pitch = atan(-accX / sqrtf(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.printf("Pitch : %.2f deg\n", my_pitch);
+  M5.Lcd.printf("Temp. : %.2f deg.C", temp);
   delay(1000);
 }
